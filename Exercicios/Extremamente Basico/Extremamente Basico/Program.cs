5520360a631d4e0c50ec4b0ecd350453e2e02b6c
Nkrumah13 <marcionkrumanh66@gmail.com>
2024-06-03 00:26:18 +0100

    g

---
+++ Exercicios/Extremamente Basico/Extremamente Basico/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extremamente_Basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            int X = A + B;
+
+            Console.WriteLine("X = " + X);
+            Console.ReadKey();
+        }
+    }
+}
