f76c09d5f56a2c2c445ebc97eb426bfe55a3352a
Nkrumah13 <marcionkrumanh66@gmail.com>
2024-06-03 03:20:23 +0100

    1

---
+++ Exercicios/Produto Simples/Produto Simples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto_Simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            int PROD = A * B;
+            Console.WriteLine("PROD = " + PROD);
+            Console.ReadKey();
+        }
+    }
+}
