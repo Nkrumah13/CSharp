d6db0e85245f84a4e32608e2a4fb93bf5a4bab47
Nkrumah13 <marcionkrumanh66@gmail.com>
2024-06-05 20:58:42 +0100

    Update

---
+++ Exercicios/Diferença/Diferença/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diferença
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+            int DIFERENCA = (A * B - C * D);
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Exercicios/Salário/Salário/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salário
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double funcionario;
+            double horas_de_trabalho;
+            double valor_hora;
+            
+            funcionario = double.Parse(Console.ReadLine());
+            horas_de_trabalho = double.Parse(Console.ReadLine());
+            valor_hora = double.Parse(Console.ReadLine());
+            double Salario = horas_de_trabalho * valor_hora;
+
+            Console.WriteLine("NUMBER = " + funcionario);
+            Console.WriteLine("SALARY = U$ " + Salario.ToString("0.00"));
+            Console.ReadKey();
+        }
+    }
+}
